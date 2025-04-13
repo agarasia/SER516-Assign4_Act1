cc40a9f6642e1c932f06ff06a6fdcb166b3be060
agarasia <agarasia@asu.edu>
2025-04-13 16:11:10 -0700

    Task-5: Modified file to violate checkstyle

--- src/main/java/edu/asu/assign4/rest/grocery/services/ProducerServices.java
+++ src/main/java/edu/asu/assign4/rest/grocery/services/ProducerServices.java
@@ -4,19 +4,28 @@ import static java.lang.Class.forName;
 
 import java.lang.reflect.Method;
 import java.util.List;
+// Adding a checkstyle violation
+import java.util.ArrayList;
 
 import edu.asu.assign4.rest.grocery.model.Producer;
 
 public interface ProducerServices {
 	public List<Producer> findAll() throws Exception;
+
 	public Producer findOne(String abbrev) throws Exception;
-	public String create(Producer p) throws Exception;		// returns the new Id
-	public boolean update(Producer p) throws Exception;   	// true if update, false if create
-	public boolean delete(String abbrev) throws Exception;			// true if id there, false if not
+
+	public String create(Producer p) throws Exception; // returns the new Id
+
+	public boolean update(Producer p) throws Exception; // true if update, false if create
+
+	public boolean delete(String abbrev) throws Exception; // true if id there, false if not
 	// Let's do a PATCH for fun
-	public boolean changeAddress(String abbrev, String newAddress) throws Exception;  // true if id found and patch done, false if not
-	
-	// normally we would implement a configurable logic to decide our service implementation
+
+	public boolean changeAddress(String abbrev, String newAddress) throws Exception; // true if id found and patch done,
+																						// false if not
+
+	// normally we would implement a configurable logic to decide our service
+	// implementation
 	public static ProducerServices getProducerService() {
 		try {
 			String __theServiceImpl = "edu.asu.assign4.rest.grocery.services.impl.SimpleProducerServicesImpl";
